102986c1ee84e1f902fd279c5a7a8072238d5bc0
Imraan Iqbal <imraaniqbal@Imraans-MacBook-Pro.local>
2023-04-26 08:33:07 -0700

    Added initital structure and implementation of the comment service

---
+++ internal/comment/comment.go
@@ -0,0 +1,64 @@
+package comment
+
+import (
+	"context"
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrFetchingComment = errors.New("failed to fetch comment by id")
+	ErrNotImplemented = errors.New("not implemented")
+)
+
+// Comment - a representation of the comment
+// structure of our service
+type Comment struct {
+	ID     string
+	Slug   string
+	Body   string
+	Author string
+}
+
+// Store - this interface defines all of the methods
+// that our service needs in order to operate
+type Store interface {
+	GetComment(context.Context, string) (Comment, error)
+}
+
+// Service - is the struct in which
+// all of our logic wil be built on
+type Service struct {
+	Store Store
+}
+
+// NewService - returns a pointer to a new
+// service
+func NewService(store Store) *Service {
+	return &Service{
+		Store: store,
+	}
+}
+
+func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
+	fmt.Println("Retreiving a comment")
+	cmt, err := s.Store.GetComment(ctx, id)
+	if err != nil {
+		fmt.Println(err)
+		return Comment{}, ErrFetchingComment
+	}
+
+	return cmt, nil
+}
+
+func (s *Service) UpdateComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) DeleteComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) CreateComment(ctx context.Context, cmt Comment) (Comment, error) {
+	return Comment{}, ErrNotImplemented
+}
